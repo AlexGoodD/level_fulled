9d22711293b53757bf407cf7bba219c20292d0a4
AlexGoodD <alejandrochavarb@gmail.com>
2025-05-05 10:55:54 -0600

    Modificacion de rutas de carga y exportacion

--- utils/compresion.py
+++ utils/compresion.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Cargar el modelo original
-modelo = tf.keras.models.load_model("modelo_nivel_llenado.h5")
+modelo = tf.keras.models.load_model("modelos/modelo_nivel_llenado.h5")
 
 # Convertir el modelo a un formato más ligero con cuantización
 converter = tf.lite.TFLiteConverter.from_keras_model(modelo)
@@ -9,7 +9,7 @@ converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 
 # Guardar el modelo comprimido
-with open("modelo_nivel_llenado.tflite", "wb") as f:
+with open("modelos/modelo_nivel_llenado.tflite", "wb") as f:
     f.write(tflite_model)
 
 print("Modelo comprimido guardado como modelo_nivel_llenado.tflite")
